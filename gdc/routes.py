35fede8fc9fde5d8190cf2ae5fd646a9a458318c
Davis Silverman <sinistersnare@gmail.com>
2015-02-05 20:48:58 -0500

    added new routes, shitty navbar

--- gdc/routes.py
+++ gdc/routes.py
@@ -4,8 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/ideas')
+def ideas():
+    return render_template("ideas.html")
+
+@app.route('/gallery')
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/resources")
+def resources():
+    return render_template("resources.html")
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.debug = True
+    app.run()
